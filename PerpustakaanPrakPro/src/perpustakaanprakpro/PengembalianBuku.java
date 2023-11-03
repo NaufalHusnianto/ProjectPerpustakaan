82e672ecc19de8f5b249ccfa1a6f88463df305b1
aaadiknh <adiknurhalimah@gmail.com>
2023-11-03 10:21:36 +0700

    GUI Pengembalian Buku

--- PerpustakaanPrakPro/src/perpustakaanprakpro/PengembalianBuku.java
+++ PerpustakaanPrakPro/src/perpustakaanprakpro/PengembalianBuku.java
@@ -26,17 +26,169 @@ public class PengembalianBuku extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        judul = new javax.swing.JLabel();
+        nama = new javax.swing.JLabel();
+        nim = new javax.swing.JLabel();
+        kodeBuku = new javax.swing.JLabel();
+        judulBuku = new javax.swing.JLabel();
+        tglPeminjam = new javax.swing.JLabel();
+        tglKembali = new javax.swing.JLabel();
+        statusKembali = new javax.swing.JLabel();
+        tagihan = new javax.swing.JLabel();
+        inputTglPeminjam = new javax.swing.JTextField();
+        inputNama = new javax.swing.JTextField();
+        inputNim = new javax.swing.JTextField();
+        inputKode = new javax.swing.JTextField();
+        inputJudul = new javax.swing.JTextField();
+        inputTglKembali = new javax.swing.JTextField();
+        inputStatus = new javax.swing.JTextField();
+        inputTagihan = new javax.swing.JTextField();
+        search = new javax.swing.JButton();
+        refresh = new javax.swing.JButton();
+        edit = new javax.swing.JButton();
+        save = new javax.swing.JButton();
+        delete = new javax.swing.JButton();
+        exit = new javax.swing.JButton();
+        tabel = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        judul.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        judul.setText("Pengembalian Buku");
+
+        nama.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        nama.setText("Nama");
+
+        nim.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        nim.setText("NIM");
+
+        kodeBuku.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        kodeBuku.setText("Kode Buku");
+
+        judulBuku.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        judulBuku.setText("Judul Buku");
+
+        tglPeminjam.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        tglPeminjam.setText("Tanggal Peminjam");
+
+        tglKembali.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        tglKembali.setText("Tanggal Kembali");
+
+        statusKembali.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        statusKembali.setText("Status Pengembalian");
+
+        tagihan.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        tagihan.setText("Tagihan");
+
+        search.setText("Search");
+
+        refresh.setText("Refresh");
+
+        edit.setText("Edit");
+
+        save.setText("Save");
+
+        delete.setText("Delete");
+
+        exit.setText("Exit");
+
+        tabel.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        tabel.setText("Tabel Pengembalian Buku");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(judul)
+                .addGap(152, 152, 152))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(tabel)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(nama)
+                            .addComponent(nim)
+                            .addComponent(kodeBuku)
+                            .addComponent(judulBuku)
+                            .addComponent(tglPeminjam)
+                            .addComponent(tglKembali)
+                            .addComponent(statusKembali)
+                            .addComponent(tagihan))
+                        .addGap(34, 34, 34)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(inputNama, javax.swing.GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
+                            .addComponent(inputNim)
+                            .addComponent(inputKode)
+                            .addComponent(inputJudul)
+                            .addComponent(inputTglPeminjam)
+                            .addComponent(inputTglKembali)
+                            .addComponent(inputStatus)
+                            .addComponent(inputTagihan))
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(search)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(refresh))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(edit)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(delete))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(save)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(exit)))))
+                .addContainerGap(21, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(judul)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nama)
+                    .addComponent(inputNama, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(search)
+                    .addComponent(refresh))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nim)
+                    .addComponent(inputNim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(edit)
+                    .addComponent(delete))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(kodeBuku)
+                    .addComponent(inputKode, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(save)
+                    .addComponent(exit))
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(judulBuku)
+                    .addComponent(inputJudul, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tglPeminjam)
+                    .addComponent(inputTglPeminjam, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tglKembali)
+                    .addComponent(inputTglKembali, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(statusKembali)
+                    .addComponent(inputStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tagihan)
+                    .addComponent(inputTagihan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(29, 29, 29)
+                .addComponent(tabel)
+                .addContainerGap(127, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,5 +230,29 @@ public class PengembalianBuku extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton delete;
+    private javax.swing.JButton edit;
+    private javax.swing.JButton exit;
+    private javax.swing.JTextField inputJudul;
+    private javax.swing.JTextField inputKode;
+    private javax.swing.JTextField inputNama;
+    private javax.swing.JTextField inputNim;
+    private javax.swing.JTextField inputStatus;
+    private javax.swing.JTextField inputTagihan;
+    private javax.swing.JTextField inputTglKembali;
+    private javax.swing.JTextField inputTglPeminjam;
+    private javax.swing.JLabel judul;
+    private javax.swing.JLabel judulBuku;
+    private javax.swing.JLabel kodeBuku;
+    private javax.swing.JLabel nama;
+    private javax.swing.JLabel nim;
+    private javax.swing.JButton refresh;
+    private javax.swing.JButton save;
+    private javax.swing.JButton search;
+    private javax.swing.JLabel statusKembali;
+    private javax.swing.JLabel tabel;
+    private javax.swing.JLabel tagihan;
+    private javax.swing.JLabel tglKembali;
+    private javax.swing.JLabel tglPeminjam;
     // End of variables declaration//GEN-END:variables
 }
