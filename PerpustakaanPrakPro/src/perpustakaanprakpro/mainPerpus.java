78c2607b86cf8f3990093c6d5d2f1da4bed28efd
Naufal Husnianto <husniantonaufal@gmail.com>
2023-11-05 11:31:58 +0700

    main class nav

--- PerpustakaanPrakPro/src/perpustakaanprakpro/mainPerpus.java
+++ PerpustakaanPrakPro/src/perpustakaanprakpro/mainPerpus.java
@@ -169,11 +169,14 @@ public class mainPerpus extends javax.swing.JFrame {
 
     private void pengembalianButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pengembalianButtonActionPerformed
         // TODO add your handling code here:
+        new PengembalianBuku().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_pengembalianButtonActionPerformed
 
     private void peminjamanButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_peminjamanButtonActionPerformed
         // TODO add your handling code here:
-        
+        new PeminjamanBuku().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_peminjamanButtonActionPerformed
 
     private void bukuButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bukuButtonActionPerformed
