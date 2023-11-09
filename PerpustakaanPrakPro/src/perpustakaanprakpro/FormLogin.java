90a366c38847d9d3b54cca287f2dab05e6572f9a
Naufal Husnianto <husniantonaufal@gmail.com>
2023-11-09 22:08:54 +0700

    login logic part1

--- PerpustakaanPrakPro/src/perpustakaanprakpro/FormLogin.java
+++ PerpustakaanPrakPro/src/perpustakaanprakpro/FormLogin.java
@@ -13,6 +13,8 @@ public class FormLogin extends javax.swing.JFrame {
     int xx, xy;
     public FormLogin() {
         initComponents();
+        
+        setLocationRelativeTo(this);
     }
 
     /**
@@ -24,6 +26,7 @@ public class FormLogin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        errorNotif = new javax.swing.JDialog();
         jPanel1 = new javax.swing.JPanel();
         image = new javax.swing.JLabel();
         inputNama = new javax.swing.JTextField();
@@ -32,6 +35,17 @@ public class FormLogin extends javax.swing.JFrame {
         inputPassword = new javax.swing.JPasswordField();
         password = new javax.swing.JLabel();
         login = new javax.swing.JButton();
+
+        javax.swing.GroupLayout errorNotifLayout = new javax.swing.GroupLayout(errorNotif.getContentPane());
+        errorNotif.getContentPane().setLayout(errorNotifLayout);
+        errorNotifLayout.setHorizontalGroup(
+            errorNotifLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        errorNotifLayout.setVerticalGroup(
+            errorNotifLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -48,8 +62,6 @@ public class FormLogin extends javax.swing.JFrame {
         });
 
         jPanel1.setBackground(new java.awt.Color(211, 239, 255));
-
-        image.setIcon(new javax.swing.ImageIcon("E:\\SEMESTER 3\\PRAKTIKUM PEMROGRAMAN\\Elements_Java\\Desain tanpa judul (6) (6).png")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -80,6 +92,11 @@ public class FormLogin extends javax.swing.JFrame {
         login.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
         login.setForeground(new java.awt.Color(255, 255, 255));
         login.setText("LOGIN");
+        login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -135,6 +152,18 @@ public class FormLogin extends javax.swing.JFrame {
         this.setLocation(x - xx, y - xy);
     }//GEN-LAST:event_formMouseDragged
 
+    private void loginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginActionPerformed
+        // TODO add your handling code here:
+        
+        if(inputNama.getText().equals("admin")) {
+            new mainPerpus().setVisible(true);
+            this.dispose();
+        } else {
+            System.out.println("password anda salah");
+        }
+        
+    }//GEN-LAST:event_loginActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -171,6 +200,7 @@ public class FormLogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDialog errorNotif;
     private javax.swing.JLabel image;
     private javax.swing.JTextField inputNama;
     private javax.swing.JPasswordField inputPassword;
